f47c9989112f911cf8898bb6294c23c645a77652
Alexia Stephan <alexia0stephan@gmail.com>
2023-12-28 17:36:34 +0100

    création du routeur

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <nav>
+      <Link to="">KASA</Link>
+    </nav>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Places/index.jsx
@@ -0,0 +1,9 @@
+function Places() {
+  return (
+    <div>
+      <h1>Logements</h1>
+    </div>
+  );
+}
+
+export default Places;
