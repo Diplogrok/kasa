d536cddb04a13e340e1e92c66420b13c0930f67b
Alexia Stephan <alexia0stephan@gmail.com>
2024-01-04 14:09:19 +0100

    intégration homepage

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,8 +1,11 @@
 import "../../assets/css/Styles.css";
+import background from "../../assets/images/background02.png";
+import jsonData from "../../assets/datas/About.json";
+console.log(jsonData);
 function About() {
   return (
-    <div>
-      <h1>👩‍💻👨‍💻👩‍💻</h1>
+    <div className="background">
+      <img src={background} alt="" className="background_img" />
     </div>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,30 @@
 import background from "../../assets/images/background01.png";
 import "../../assets/css/Styles.css";
-import jsonData from "../../assets/datas/Data.json";
+import jsonData from "../../assets/datas/Logements.json";
 console.log(jsonData);
+
 function Home() {
   const data = jsonData;
 
   return (
     <div>
-      <img src={background} alt="" className="" />{" "}
+      <div className="background">
+        <img src={background} alt="" className="background_img" />
+        <h1 className="background_txt">Chez vous, partout et ailleurs</h1>
+      </div>
       {data && (
-        <ul>
+        <div className="card">
           {data.map((item) => (
-            <li key={item.id}>{item.name}</li>
+            <div className="card_detail" key={item.id}>
+              <img
+                className="card_detail-img"
+                src={item.cover}
+                alt={item.title}
+              />
+              <h3 className="card_detail-txt">{item.title}</h3>
+            </div>
           ))}
-        </ul>
+        </div>
       )}
     </div>
   );
