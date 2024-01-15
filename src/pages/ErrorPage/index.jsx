823827cf0297956b89819564212dcf9622cf2156
Alexia Stephan <alexia0stephan@gmail.com>
2024-01-15 11:54:50 +0100

    réorganisation des fichiers scss

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import Error from "../../components/Error";
+import { NavLink } from "react-router-dom";
 import "../../assets/css/Error.css";
 
 function ErrorPage() {
   return (
-    <div>
-      <Error />
+    <div className="error">
+      <h1 className="error_title">404</h1>
+      <p className="error_subtitle">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
+        Retourner sur la page d'accueil
+      </NavLink>
     </div>
   );
 }
